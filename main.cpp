6d00acde42f0587fffaad90f055ba3abdebe205b
Khayal H <43758778+khmaxim@users.noreply.github.com>
2020-12-23 21:32:35 +0400

    Implement PQ with binary heap structure
    Develop a simulation for the taxi application
    Use min-heapify to get minimum

--- main.cpp
+++ main.cpp
@@ -7,38 +7,95 @@
 #include <time.h> // time
 #include <vector> // vector
 #include <algorithm> // swap
+#include <limits> // numeric_limits<float>::max()
 
 using namespace std;
 
 class PQ {
 private:
-	vector<float> heap; // holds taxi distances in heap data structure
+	vector<long double> heap; // holds taxi distances in heap data structure
+	int getParentIndex(int index);
 public:
 	PQ() {}
 	~PQ() {}
-	void addTaxi(float longitude, float latitude); // adds taxi to the heap
-	float calculateDistance(float longitude, float latitude); // Euclidian distance
-	void updateDistance(int key, float subtrahend); // updates variables and calls calculateDistance()
+	void addTaxi(long double longitude, long double latitude); // adds taxi to the heap
+	long double calculateDistance(long double longitude, long double latitude); // Euclidian distance
+	void updateDistance(int index, long double subtrahend); // updates variables and calls calculateDistance()
 	int getSize(); // returns size of heap data struct
+	void min_heapify(int index); // heapify the interior node
+	//void build_min_heap(); // heapify all interior nodes
+	long double extract_min(); // extract head node (which is closest) and return its distance
+	void decrease_key(int index, long double key);
+	long double getKey(int index);
 };
 
-void PQ::addTaxi(float longitude, float latitude) {
+long double PQ::getKey(int index) {
+	return this->heap[index];
+}
+
+int PQ::getParentIndex(int index) {
+	return (index-1) / 2;
+}
+
+void PQ::decrease_key(int index, long double key) {
+	if (key > this->heap[index]) {
+		cout << "New key is bigger than the value" << endl;
+		return;
+	}
+	this->heap[index] = key;
+	while(index > 1 && this->heap[this->getParentIndex(index)] > this->heap[index]) { // if parent is bigger than child
+		swap(this->heap[index], this->heap[this->getParentIndex(index)]); // swap parent and child
+		index = this->getParentIndex(index); // check the parent of the parent
+	}
+}
+
+long double PQ::extract_min() {
+	long double min = 0.0;
+	if (this->getSize() == 0) {
+		throw "Heap data structure is empty";
+	}
+	min = this->heap[0];
+	swap(this->heap[0], this->heap[this->getSize() - 1]); // swap first and last nodes
+	this->heap.pop_back(); // remove the last element
+	min_heapify(0); // heapify the new root
+	return min;
+}
+
+void PQ::min_heapify(int index) {
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
+
+	int smallest = index;
+
+	if (left < this->getSize() && this->heap[left] < this->heap[index])
+		smallest = left;
+
+	if (right < this->getSize() && this->heap[right] < this->heap[smallest])
+		smallest = right;
+
+	if (smallest != index) {
+		swap(this->heap[index], this->heap[smallest]);
+		min_heapify(smallest);
+	}
+}
+
+void PQ::addTaxi(long double longitude, long double latitude) {
 	// add new taxi's distance to the end of heap
-	this->heap.push_back(calculateDistance(longitude, latitude));
+	long double key = calculateDistance(longitude, latitude); // add new taxi to the end
+	this->heap.push_back(numeric_limits<long double>::max());
+	this->decrease_key(this->getSize() - 1, key);
+	//min_heapify(this->getSize() - 1); // swap it into its correct place
 }
 
 int PQ::getSize() {
 	return this->heap.size(); // return heap size
 }
 
-float PQ::calculateDistance(float longitude, float latitude) {
-	float distance = sqrt(pow(longitude, 2) + pow(latitude, 2));
+long double PQ::calculateDistance(long double longitude, long double latitude) {
+	long double distance = sqrtl(powl(longitude, 2) + powl(latitude, 2));
 	return distance; // calculate and return Euclidian distance
 }
 
-void PQ::updateDistance(int key, float subtrahend) { // should be between 0 and 1
-	this->heap[key] -= subtrahend; // reduce distance by subtrahend
-}
 
 int main(int argc, char** argv){
 	clock_t time_elapsed; // used for storing total running time
@@ -89,21 +146,35 @@ int main(int argc, char** argv){
 	int num_of_distance_updates = 0;
 
 	for (int i = 0; i < m; i++) { // do m amount of operations
-		if(i == 100) { // after every 100 operations, 101th operation is a remove operation
-			// remove taxi
-			// print removed taxi
+		if(i % 100 == 0 && i != 0) { // after every 100 operations, 101th operation is a remove operation
+			try {
+				long double taxi_distance = queue->extract_min(); // remove taxi
+				cout << "Distance of the removed taxi: " << taxi_distance << endl; // print removed taxi
+			}
+    		catch (const char* result) {
+        		//cout << result << endl;
+    		}
 		} else {
 			mode = (rand() % 100) < (p * 100);
 			if(mode == true) {
 				// update taxi (distance = distance * 0.1)
+				if(queue->getSize() == 0) {
+                    //cout << "Can't update non-existent taxi" << endl;
+                    continue;
+                }
 				num_of_distance_updates++;
-
-				queue->updateDistance(rand() % queue->getSize(), 0.01); // reduce a random taxi's distance by 0.01
+				int index = rand() % queue->getSize(); // randomly choose a node
+				long double reduced_value = queue->getKey(index) - 0.01;
+				if(reduced_value <= 0.0) {
+					queue->decrease_key(index, queue->getKey(index) - 0.0); // distance can't be negative so reduce a random taxi's distance to 0.0
+				} else {
+					queue->decrease_key(index, reduced_value); // reduce a random taxi's distance by 0.01
+				}
 			} else {
 				// add taxi
 				num_of_taxi_additions++;
 				
-				float longitude, latitude;
+				long double longitude, latitude;
 				file >> longitude;
 				file >> latitude;
 				getline(file, line, '\n'); //this is for reading the \n character into dummy variable.
